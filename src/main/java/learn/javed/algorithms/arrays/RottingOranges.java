8685ad68ae6cc48e6f805ac6e1ead8e584941988
Javed Attar <jattar@financialengines.com>
2021-10-30 15:21:40 -0700

    Approach without node element

--- src/main/java/learn/javed/algorithms/arrays/RottingOranges.java
+++ src/main/java/learn/javed/algorithms/arrays/RottingOranges.java
@@ -29,6 +29,9 @@ public class RottingOranges {
 		RottingOranges rottingOranges = new RottingOranges();
 		System.out.println("Rotting Oranges grid >> " + Arrays.deepToString(grid) + " rots in ==> "
 				+ rottingOranges.orangesRotting(grid));
+		grid = new int[][] { { 2, 1, 1 }, { 1, 1, 0 }, { 0, 1, 1 } };
+		System.out.println("Rotting Oranges 2 grid >> " + Arrays.deepToString(grid) + " rots in ==> "
+				+ rottingOranges.orangesRotting2(grid));
 		grid = new int[][] { { 2, 1, 1 }, { 0, 1, 1 }, { 1, 0, 1 } };
 		System.out.println("Rotting Oranges grid >> " + Arrays.deepToString(grid) + " rots in ==> "
 				+ rottingOranges.orangesRotting(grid));
@@ -127,6 +130,79 @@ public class RottingOranges {
 		public int getY() {
 			return y;
 		}
+
+	}
+
+	public int orangesRotting2(int[][] grid) {
+		/**
+		 * An orange will be rotten only when adjacent one rotten. In other words oranges on 4 side of rotten orange
+		 * will rotten in first minute,
+		 * Second minute all fresh oranges adjacent previous 4 will rotten hence this falls under level by level check
+		 */
+		int minutes = 0;
+		int freshCount = 0;
+		Queue<Integer> rottingQueue = new LinkedList<>();
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				switch (grid[i][j]) {
+				case 1:
+					freshCount++;
+					break;
+				case 2:
+					rottingQueue.add(i * (grid[i].length) + (j + 1));
+					break;
+				}
+				if (grid[i][j] == 1) {
+
+				}
+			}
+		}
+		if (freshCount == 0)
+			return 0;
+
+		int rottenOrangesSize = rottingQueue.size();
+		while (!rottingQueue.isEmpty() && rottenOrangesSize > 0) {
+			Integer ro = rottingQueue.poll();
+			// find adjacent for x, y => [x,y-1], [x,y+1], [x-1, y], [x+1, y]
+			// see if any of them are fresh oranges, reset them to rotten and add to queue
+
+			// Left node
+			int y = ro % (grid[0].length);
+			int x = ro - y / (grid[0].length);
+
+			if (y - 1 >= 0 && grid[x][y - 1] == 1) {
+				rottingQueue.add(ro - 1);
+				grid[x][y - 1] = 2;
+				freshCount--;
+			}
+			// Right node
+			if (y + 1 < grid[0].length && grid[x][y + 1] == 1) {
+				rottingQueue.add(ro + 1);
+				grid[x][y + 1] = 2;
+				freshCount--;
+			}
+			// Top Node
+			if (x - 1 >= 0 && grid[x - 1][y] == 1) {
+				rottingQueue.add(ro - grid[0].length);
+				grid[x - 1][y] = 2;
+				freshCount--;
+			}
+			// Bottom Node
+			if (x + 1 < grid.length && grid[x + 1][y] == 1) {
+				rottingQueue.add(ro + grid[0].length);
+				grid[x + 1][y] = 2;
+				freshCount--;
+			}
+			rottenOrangesSize--;
+
+			if (!rottingQueue.isEmpty() && rottenOrangesSize == 0) {
+				rottenOrangesSize = rottingQueue.size();
+				minutes++;
+			}
+
+		}
+
+		return freshCount != 0 ? -1 : minutes;
 
 	}
 
