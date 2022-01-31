065dcfc59b0bc908cf07c2afac9106631120a1e5
Javed Attar <javed,attar@gmail.com>
2022-01-31 12:47:54 -0800

    Shorttest path and one edit distance

---
+++ src/main/java/learn/javed/algorithms/strings/OneEditDistance.java
@@ -0,0 +1,32 @@
+package learn.javed.algorithms.strings;
+
+public class OneEditDistance {
+
+	public static void main(String[] args) {
+		System.out.println(isOneDistanceApart("a", "b"));
+		System.out.println(isOneDistanceApart("", ""));
+		System.out.println(isOneDistanceApart("a", "ab"));
+		System.out.println(isOneDistanceApart("ab", "acb"));
+	}
+
+	private static boolean isOneDistanceApart(String s, String t) {
+		
+		int[][] dp = new int[s.length()+1][t.length()+1];
+		
+		for (int i = 1; i < dp.length; i++) {
+			for (int j = 1; j < dp[0].length; j++) {
+				if(s.charAt(i-1) == t.charAt(j-1) ) {
+					dp[i][j] = dp[i-1][j-1];
+				} else {
+					dp[i][j] = 1+ Math.min(dp[i][j-1], Math.min(dp[i-1][j-1], dp[i-1][j]));
+				}
+			}
+		}
+		return dp[dp.length-1][dp[0].length-1] == 1;
+	}
+	
+	private static boolean isOneDistanceApart2(String s, String t) {
+		return false;
+	}
+
+}
