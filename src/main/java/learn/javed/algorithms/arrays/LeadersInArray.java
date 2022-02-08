102b51e423c7d7c12685250f715e722bc59150e8
Javed Attar <javed,attar@gmail.com>
2022-02-07 17:13:56 -0800

    New algos

---
+++ src/main/java/learn/javed/algorithms/arrays/LeadersInArray.java
@@ -0,0 +1,39 @@
+package learn.javed.algorithms.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeadersInArray {
+
+    /**
+     * element is leader if there are no elements larger on right side. last element is always a leader
+     */
+    public static void main(String[] args) {
+        int[] array = {88, 32, 44, 23, 57, 22, 33, 44, 55};
+
+        new LeadersInArray().printLeaders(array).forEach(System.out::println);
+    }
+
+    /**
+     * Time: O[n]
+     * Space: O[1]
+     */
+    public List<Integer> printLeaders(int[] array) {
+        List<Integer> leaders = new ArrayList<>();
+
+        if (array == null || array.length == 0)
+            return leaders;
+
+        int currentLeader = array[array.length - 1];
+        leaders.add(currentLeader);
+
+        for (int i = array.length - 2; i >= 0; i--) {
+            if (array[i] > currentLeader) {
+                currentLeader = array[i];
+                leaders.add(currentLeader);
+            }
+        }
+
+        return leaders;
+    }
+}
--- src/main/java/learn/javed/algorithms/strings/OneEditDistance.java
+++ src/main/java/learn/javed/algorithms/strings/OneEditDistance.java
@@ -7,6 +7,10 @@ public class OneEditDistance {
 		System.out.println(isOneDistanceApart("", ""));
 		System.out.println(isOneDistanceApart("a", "ab"));
 		System.out.println(isOneDistanceApart("ab", "acb"));
+		System.out.println(isOneDistanceApart2("abc", "abx"));
+		System.out.println(isOneDistanceApart2("abc", "abcx"));
+		System.out.println(isOneDistanceApart2("ab", "acb"));
+		System.out.println(isOneDistanceApart2("", ""));
 	}
 
 	private static boolean isOneDistanceApart(String s, String t) {
@@ -26,7 +30,29 @@ public class OneEditDistance {
 	}
 	
 	private static boolean isOneDistanceApart2(String s, String t) {
-		return false;
+		if (s == null && t == null) return true;
+
+        int len1 = s.length();
+        int len2 = t.length();
+
+        if (s.equals(t)) return false;
+
+        if (len1 > len2)
+            return isOneDistanceApart2(t, s);
+
+        for (int i = 0; i < len1; i++) {
+            // if current chars not same, for same len string, we can exclude these chars and rest of the string should match
+            // Otherwise, as first string is smaller than second one, we just need to skip char from second string
+            // and match with current remaining string
+            if (s.charAt(i) != t.charAt(i)) {
+                if (len1 == len2)
+                    return s.substring(i + 1).equals(t.substring(i + 1));
+                else
+                    return s.substring(i).equals(t.substring(i + 1));
+            }
+        }
+
+        return true;
 	}
 
 }
